22ca8ca345e3ad6a4424c3279b8b59aba54b51c3
adityaShar24 <adityarajansharma1234@gmail.com>
2023-08-07 17:00:03 +0530

    add user model

--- src/database/mongo.py
+++ src/database/mongo.py
@@ -1,9 +1,13 @@
 from pymongo import MongoClient
 from utils.constants import CONNECTED_TO_MONGODB , CONNECTION_FAILED
 
-MONGO_CONNECTION_STRING = ''
+MONGO_CONNECTION_STRING = 'mongodb+srv://aditya:aditya2004@cluster0.lgjqzvz.mongodb.net/?retryWrites=true&w=majority'
 
 mongo_client = MongoClient(MONGO_CONNECTION_STRING)
+
+database = mongo_client['Cricket-quiz']
+users_collection = database['Users']
+
 
 try:
     mongo_client.server_info()
