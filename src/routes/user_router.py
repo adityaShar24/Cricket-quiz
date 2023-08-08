476b7d0987a2dcb83aeac36bdda1f2f3527acd72
adityaShar24 <adityarajansharma1234@gmail.com>
2023-08-08 11:11:35 +0530

    added login route

--- src/routes/user_router.py
+++ src/routes/user_router.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from controllers.user_controller import register
+from controllers.user_controller import register , login
 from flask_caching import Cache
 
 cache = Cache()
@@ -8,4 +8,10 @@ auth_bp = Blueprint('auth_bp' , __name__)
 @auth_bp.post('/register')
 def register_user_wrapper():
     return register()
+
+@auth_bp.post('/login')
+@cache.cached(timeout=15)
+def login_user_wrapper():
+    return login
+    
 
