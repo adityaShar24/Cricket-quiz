00a12de141777e2e36924a0b87e847d3a5ac17ee
Aditya Sharma <110603539+adityaShar24@users.noreply.github.com>
2023-08-10 12:34:00 +0000

    add is admin

--- src/models/user_model.py
+++ src/models/user_model.py
@@ -1,5 +1,5 @@
 from database.mongo import users_collection
-
+from bson.objectid import ObjectId
 class User:
     def __init__(self , username , password , role='basic'):
         self.username = username
@@ -13,6 +13,11 @@ class User:
     def find_by_username(username):
         user = users_collection.find_one({'username':username})
         return user
+    
+    def is_admin(userId):
+        user_admin = users_collection.find_one({"_id": ObjectId(userId)})
+        if user_admin['role'] == 'admin':
+            return user_admin
     
     def find_all_users():
         users = users_collection.find()
