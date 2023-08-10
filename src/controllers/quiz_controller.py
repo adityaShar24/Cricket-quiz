7350f46dc4d84b0be049b62029e42381c832b500
Aditya Sharma <110603539+adityaShar24@users.noreply.github.com>
2023-08-10 12:33:02 +0000

    add quiz controller

--- src/controllers/quiz_controller.py
+++ src/controllers/quiz_controller.py
@@ -1,14 +1,20 @@
-from flask import request , make_response , json
+from flask import request, make_response, json
 from models.quiz_model import Question
-from utils.constants import HTTP_201_CREATED , QUESTION_CREATED_MESSAGE
+from utils.constants import HTTP_201_CREATED, QUESTION_CREATED_MESSAGE , ACCESS_FORBIDDEN , HTTP_405_FORBIDDEN
 import bson.json_util as json_util
+from models.user_model import User
 
 def create_question():
     body = json.loads(request.data)
+    user_id = body['userId']
+    is_admin = User.is_admin(user_id)
+    if not is_admin:
+        return make_response({"message": ACCESS_FORBIDDEN}, HTTP_405_FORBIDDEN)
+    
     question = body['question']
     options = body['options']
     correct_option = body['correct_option']
-    questions = Question(question , options , correct_option)
+    questions = Question(question, options, correct_option, user_id)
     saved_questions = questions.save_question()
     json_version = json_util.dumps(saved_questions)
-    return make_response({'message': QUESTION_CREATED_MESSAGE , 'question': json_version} , HTTP_201_CREATED)
+    return make_response({'message': QUESTION_CREATED_MESSAGE, 'question': json_version}, HTTP_201_CREATED)
