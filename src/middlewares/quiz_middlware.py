d5e3bfe2b36c9e8b6e8a02723be330a8c2302a61
Aditya Sharma <110603539+adityaShar24@users.noreply.github.com>
2023-08-10 12:26:11 +0000

    refactor

--- src/middlewares/quiz_middlware.py
+++ src/middlewares/quiz_middlware.py
@@ -1,16 +1,19 @@
-from flask import make_response , request , json
-from utils.constants import QUESTION_INPUT_MISSING_ERROR , HTTP_400_BAD_REQUEST , OPTIONS_MISSING_ERROR , CORRECT_OPTION_MISSING_ERROR
-
-
+from flask import json , make_response , request
+from utils.constants import HTTP_400_BAD_REQUEST , QUESTION_INPUT_MISSING_ERROR , OPTIONS_MISSING_ERROR , CORRECT_OPTION_MISSING_ERROR , CREATE_QUESTION_ENDPOINT , USER_ID_MISSING_ERROR
 
 def create_question_middleware():
-    body = json.loads(request.data)
-    question = body['question']
-    options = body['options']
-    correct_option = body['correct_option']
-    if not question:
-        return make_response({'message': QUESTION_INPUT_MISSING_ERROR} , HTTP_400_BAD_REQUEST)
-    if not options:
-        return make_response({'message': OPTIONS_MISSING_ERROR} , HTTP_400_BAD_REQUEST)
-    if not correct_option:
-        return make_response({'message': CORRECT_OPTION_MISSING_ERROR} , HTTP_400_BAD_REQUEST)
+    if request.endpoint == CREATE_QUESTION_ENDPOINT:
+        body = json.loads(request.data)
+        question = body['question']
+        options = body['options']
+        correct_option = body['correct_option']
+        userId = body['userId']
+        
+        if not userId:
+            return make_response({'message': USER_ID_MISSING_ERROR} , HTTP_400_BAD_REQUEST)
+        if not question:
+            return make_response({'message': QUESTION_INPUT_MISSING_ERROR} , HTTP_400_BAD_REQUEST)
+        if not options:
+            return make_response({'message': OPTIONS_MISSING_ERROR} , HTTP_400_BAD_REQUEST)
+        if not correct_option:
+            return make_response({'message': CORRECT_OPTION_MISSING_ERROR} , HTTP_400_BAD_REQUEST)
