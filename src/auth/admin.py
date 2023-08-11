add1e8e4bab9f84d4d7ccacffaea873f00645f07
Aditya Sharma <110603539+adityaShar24@users.noreply.github.com>
2023-08-11 09:19:05 +0000

    refactor

--- src/auth/admin.py
+++ src/auth/admin.py
@@ -1,19 +1,21 @@
 from flask import make_response
 from flask_jwt_extended import get_jwt
+from models.user_model import User
 from functools import wraps
 
-def admin_only():
+
+def admin_only(role):
     def wrapper(fn):
         @wraps(fn)
         def decorator(*args, **kwargs):
-            result = get_jwt()
-            print(result)
-            username = result["sub"]
-            print(username)
-            if username == "aditya":
+            user = User.is_admin(role)
+            print(user)
+            
+            if user['role'] == 'admin':
                 return fn(*args, **kwargs)
             else:
-                return make_response({ "message": "You are not allowed to access this endpoint." }, 405)
+                return make_response({"message": "You are not allowed to access this endpoint."}, 405)
     
         return decorator
     return wrapper
+
