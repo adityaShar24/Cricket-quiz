7ffc6426e4fd20039c2d989af0b08df10b677658
Aditya Sharma <110603539+adityaShar24@users.noreply.github.com>
2023-08-11 09:11:17 +0000

    add Answer

--- src/models/quiz_model.py
+++ src/models/quiz_model.py
@@ -1,4 +1,4 @@
-from database.mongo import questions_collection
+from database.mongo import questions_collection , answers_collection
 from bson.objectid import ObjectId
 class Question:
     def __init__(self , question, options , correct_option , userId):
@@ -11,3 +11,12 @@ class Question:
         question_id = questions_collection.insert_one({'question':self.question , 'options': self.options , 'correct_option': self.correct_option , 'userId':self.userId}).inserted_id
         return question_id
     
+class Answer():
+    def __init__(self , questionId , selected_option , userId):
+        self.questionId = ObjectId(questionId)
+        self.selected_option = selected_option
+        self.userId = ObjectId(userId)
+        
+    def save_answer(self):
+        answer_id = answers_collection.insert_one({'questionId':self.questionId , 'selected_option':self.selected_option , 'userId':self.userId}).inserted_id
+        return answer_id
