68ef42879a602891cf27844e519a6c306ebb69b7
Aditya Sharma <110603539+adityaShar24@users.noreply.github.com>
2023-08-12 09:06:02 +0000

    add submit answer route

--- src/routes/quiz_router.py
+++ src/routes/quiz_router.py
@@ -1,12 +1,21 @@
-from flask import Blueprint
-from controllers.quiz_controller import create_question
+from flask import Blueprint , render_template
+from controllers.quiz_controller import create_question , submit_answer
+
 from flask_jwt_extended import jwt_required
 from auth.admin import admin_only
 
 quiz_bp = Blueprint('quiz_bp' , __name__)
 
 @quiz_bp.post('/create_question')
-@jwt_required()
-@admin_only()
+# @jwt_required()
+# @admin_only(role='admin')
 def create_question_wrapper():
     return create_question()
+
+@quiz_bp.route('/create_question.html', methods =['POST' , 'GET'])
+def render_create_question():
+    return render_template('./create_question.html')
+
+@quiz_bp.post('/submit_answer')
+def submit_answer_wrapper():
+    return submit_answer()
