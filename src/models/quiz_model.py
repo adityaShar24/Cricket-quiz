494af80f5a9bef7827f376191f58895176ef66a3
Aditya Sharma <110603539+adityaShar24@users.noreply.github.com>
2023-08-14 10:42:15 +0000

    refactor

--- src/models/quiz_model.py
+++ src/models/quiz_model.py
@@ -1,14 +1,13 @@
 from database.mongo import questions_collection , answers_collection
 from bson.objectid import ObjectId
 class Question:
-    def __init__(self , question, options , correct_option , userId):
+    def __init__(self , question, options , correct_option):
         self.question = question 
         self.options = options
         self.correct_option = correct_option
-        self.userId = ObjectId(userId)
         
     def save_question(self):
-        question_id = questions_collection.insert_one({'question':self.question , 'options': self.options , 'correct_option': self.correct_option , 'userId':self.userId}).inserted_id
+        question_id = questions_collection.insert_one({'question':self.question , 'options': self.options , 'correct_option': self.correct_option}).inserted_id
         return question_id
     
 class Answer():
