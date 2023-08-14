18278a44e6a60cca7ac1832ea6d76bda14988fc2
Aditya Sharma <110603539+adityaShar24@users.noreply.github.com>
2023-08-14 10:40:53 +0000

    add user get_jwt

--- src/controllers/quiz_controller.py
+++ src/controllers/quiz_controller.py
@@ -1,21 +1,26 @@
 # controller.py
 from flask import request, make_response, json
 from models.quiz_model import Question , Answer
+from flask_jwt_extended import get_jwt
 from utils.constants import HTTP_201_CREATED, QUESTION_CREATED_MESSAGE , ACCESS_FORBIDDEN , HTTP_405_FORBIDDEN , CORRECT_OPTION_MESSAGE , INCORRECT_OPTION_MESSAGE
 import bson.json_util as json_util
 from models.user_model import User
 
 def create_question():
     body = json.loads(request.data)
-    user_id = body['userId']
+    
     question = body['question']
     options = body['options']
     correct_option = body['correct_option']
-    is_admin = User.is_admin(user_id)
+    
+    user_id = get_jwt()
+    is_admin = User.is_admin(user_id["sub"])
     if not is_admin:
         return make_response({"message": ACCESS_FORBIDDEN}, HTTP_405_FORBIDDEN)
-    questions = Question(question, options, correct_option, user_id)
+    
+    questions = Question(question, options, correct_option)
     saved_questions = questions.save_question()
+    
     json_version = json_util.dumps(saved_questions)
     return make_response({'message': QUESTION_CREATED_MESSAGE, 'question': json_version}, HTTP_201_CREATED)
 
