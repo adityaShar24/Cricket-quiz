ef3ed09c6efe49902fe84172510cf113a506ba4c
Aditya Sharma <110603539+adityaShar24@users.noreply.github.com>
2023-08-14 10:37:54 +0000

    refactor

--- src/middlewares/quiz_middleware.py
+++ src/middlewares/quiz_middleware.py
@@ -7,14 +7,13 @@ def create_question_middleware():
         question = body['question']
         options = body['options']
         correct_option = body['correct_option']
-        userId = body['userId']
         
-        if not userId:
-            return make_response({'message': USER_ID_MISSING_ERROR} , HTTP_400_BAD_REQUEST)
         if not question:
             return make_response({'message': QUESTION_INPUT_MISSING_ERROR} , HTTP_400_BAD_REQUEST)
+        
         if not options:
             return make_response({'message': OPTIONS_MISSING_ERROR} , HTTP_400_BAD_REQUEST)
+        
         if not correct_option:
             return make_response({'message': CORRECT_OPTION_MISSING_ERROR} , HTTP_400_BAD_REQUEST)
         
