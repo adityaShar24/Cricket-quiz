4786365afac543c1250390bf07dbe9b8a23d60b1
Aditya Sharma <110603539+adityaShar24@users.noreply.github.com>
2023-08-18 06:17:01 +0000

    add start quiz route

--- src/routes/quiz_router.py
+++ src/routes/quiz_router.py
@@ -1,5 +1,5 @@
 from flask import Blueprint , render_template
-from controllers.quiz_controller import create_question , submit_answer
+from controllers.quiz_controller import create_question , submit_answer , random_questions
 
 from flask_jwt_extended import jwt_required
 from auth.admin import admin_only
@@ -19,3 +19,7 @@ def render_create_question():
 @quiz_bp.post('/submit_answer')
 def submit_answer_wrapper():
     return submit_answer()
+
+@quiz_bp.get('/start_quiz')
+def get_questions_wrapper():
+    return random_questions()
