bee60708d81059b4e1811bbdeb66a64cc9e25b52
Aditya Sharma <110603539+adityaShar24@users.noreply.github.com>
2023-09-07 11:22:31 +0000

    add routes

--- src/routes/quiz_router.py
+++ src/routes/quiz_router.py
@@ -1,30 +1,31 @@
 from flask import Blueprint , render_template
 from controllers.quiz_controller import create_question , submit_answer , random_questions
-
 from flask_jwt_extended import jwt_required
 from auth.admin import admin_only
 
 quiz_bp = Blueprint('quiz_bp' , __name__)
 
+
+#Backend Routes
 @quiz_bp.post('/create_question')
 @jwt_required()
-@admin_only(role='admin')
 def create_question_wrapper():
     return create_question()
 
-@quiz_bp.route('/create_question.html', methods =['POST' , 'GET'])
-def render_create_question():
-    return render_template('./create_question.html')
 
 @quiz_bp.post('/submit_answer')
-def submit_answer_wrapper():
+def ssubmit_answer_wrapper():
     return submit_answer()
 
 @quiz_bp.get('/questions')
 def get_questions_wrapper():
     return random_questions()
 
-
-@quiz_bp.get('/start_quiz')
+#Frontend Routes
+@quiz_bp.route('/start_quiz' , methods =['GET'])
 def get_random_questions_wrapper():
-    return random_questions()
+    return render_template("./start_quiz.html")
+
+@quiz_bp.route('/create_question', methods =['POST' , 'GET'])
+def render_create_question():
+    return render_template('./create_question.html')
--- src/routes/user_router.py
+++ src/routes/user_router.py
@@ -1,10 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint , render_template
 from controllers.user_controller import register , login , get_all_users
 from flask_caching import Cache
 from flask_jwt_extended import jwt_required
 cache = Cache()
 auth_bp = Blueprint('auth_bp' , __name__)
 
+
+#Backend Routes
 @auth_bp.post('/register')
 def register_user_wrapper():
     return register()
@@ -18,3 +20,18 @@ def login_user_wrapper():
 @jwt_required()
 def get_all_users_wrapper():
     return get_all_users()
+
+
+#Frontend Routes
+@auth_bp.route('/', methods =['POST' , 'GET'])
+def render_main():
+    return render_template('./home.html')
+
+@auth_bp.route('/register', methods =['POST' , 'GET'])
+def render_register():
+    return render_template('./register.html')
+
+@auth_bp.route('/login', methods =['POST' , 'GET'])
+def render_login():
+    return render_template('./login.html')
+
